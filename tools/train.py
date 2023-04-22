c2abdb1f587892da38c3eb79af64563c81a63700
Bảnh <83081141+Banhkun@users.noreply.github.com>
2023-04-22 16:39:14 +0700

    Update train.py

--- tools/train.py
+++ tools/train.py
@@ -3,7 +3,8 @@ import argparse
 import logging
 import os
 import os.path as osp
-
+import warnings
+warnings.filterwarnings("ignore", category=RuntimeWarning) 
 from mmengine.config import Config, DictAction
 from mmengine.logging import print_log
 from mmengine.registry import RUNNERS
