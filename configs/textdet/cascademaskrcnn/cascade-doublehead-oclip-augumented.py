c89b25e5666bbe53109b34cb12c45f61241a53e2
Bảnh <83081141+Banhkun@users.noreply.github.com>
2023-04-24 18:15:35 +0700

    Update cascade-doublehead-oclip-augumented.py

--- configs/textdet/cascademaskrcnn/cascade-doublehead-oclip-augumented.py
+++ configs/textdet/cascademaskrcnn/cascade-doublehead-oclip-augumented.py
@@ -1,5 +1,4 @@
 file_client_args = dict(backend='disk')
-
 model = dict(
     type='MMDetWrapper',
     text_repr_type='poly',
@@ -12,197 +11,211 @@ model = dict(
             bgr_to_rgb=True,
             pad_mask=False,
             pad_size_divisor=32),
-        backbone = dict(
-    _scope_='mmocr',
-    type='CLIPResNet',
-    init_cfg=dict(
-        type='Pretrained',
-        checkpoint='https://download.openmmlab.com/'
-        'mmocr/backbone/resnet50-oclip-7ba0c533.pth')),
-    neck=dict(
-        type='FPN',
-        in_channels=[256, 512, 1024, 2048],
-        out_channels=256,
-        num_outs=5),
-    rpn_head=dict(
-        type='RPNHead',
-        in_channels=256,
-        feat_channels=256,
-        anchor_generator=dict(
-            type='AnchorGenerator',
-            scales=[8],
-            ratios=[0.5, 1.0, 2.0],
-            strides=[4, 8, 16, 32, 64]),
-        bbox_coder=dict(
-            type='DeltaXYWHBBoxCoder',
-            target_means=[.0, .0, .0, .0],
-            target_stds=[1.0, 1.0, 1.0, 1.0]),
-        loss_cls=dict(
-            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
-        loss_bbox=dict(type='SmoothL1Loss', beta=1.0 / 9.0, loss_weight=1.0)),
-    roi_head=dict(
-        type='CascadeRoIHead',
-        reg_roi_scale_factor=1.3,
-        num_stages=3,
-        stage_loss_weights=[1, 0.5, 0.25],
-        bbox_roi_extractor=dict(
-            type='SingleRoIExtractor',
-            roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
+        backbone=dict(
+            _scope_='mmocr',
+            type='CLIPResNet',
+            init_cfg=dict(
+                type='Pretrained',
+                checkpoint=
+                'https://download.openmmlab.com/mmocr/backbone/resnet50-oclip-7ba0c533.pth'
+            )),
+        neck=dict(
+            type='FPN',
+            in_channels=[256, 512, 1024, 2048],
             out_channels=256,
-            featmap_strides=[4, 8, 16, 32]),
-        bbox_head=[
-            dict(
-                type='DoubleConvFCBBoxHead',
-                num_convs=4,
-                num_fcs=2,
-                in_channels=256,
-                conv_out_channels=1024,
-                fc_out_channels=1024,
-                roi_feat_size=7,
-                num_classes=1,
-                bbox_coder=dict(
-                    type='DeltaXYWHBBoxCoder',
-                    target_means=[0., 0., 0., 0.],
-                    target_stds=[0.1, 0.1, 0.2, 0.2]),
-                reg_class_agnostic=True,
-                loss_cls=dict(
-                    type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
-                loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0)),
-            dict(
-                type='DoubleConvFCBBoxHead',
-                num_convs=4,
-                num_fcs=2,
-                in_channels=256,
-                conv_out_channels=1024,
-                fc_out_channels=1024,
-                roi_feat_size=7,
-                num_classes=1,
-                bbox_coder=dict(
-                    type='DeltaXYWHBBoxCoder',
-                    target_means=[0., 0., 0., 0.],
-                    target_stds=[0.05, 0.05, 0.1, 0.1]),
-                reg_class_agnostic=True,
-                loss_cls=dict(
-                    type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
-                loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0)),
-            dict(
-                type='DoubleConvFCBBoxHead',
-                num_convs=4,
-                num_fcs=2,
-                in_channels=256,
-                conv_out_channels=1024,
-                fc_out_channels=1024,
-                roi_feat_size=7,
-                num_classes=1,
-                bbox_coder=dict(
-                    type='DeltaXYWHBBoxCoder',
-                    target_means=[0., 0., 0., 0.],
-                    target_stds=[0.033, 0.033, 0.067, 0.067]),
-                reg_class_agnostic=True,
-                loss_cls=dict(
-                    type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
-                loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0))
-        ],
-        mask_roi_extractor=dict(
-            type='SingleRoIExtractor',
-            roi_layer=dict(type='RoIAlign', output_size=14, sampling_ratio=0),
-            out_channels=256,
-            featmap_strides=[4, 8, 16, 32]),
-        mask_head=dict(
-            type='FCNMaskHead',
-            num_convs=4,
+            num_outs=5),
+        rpn_head=dict(
+            type='RPNHead',
             in_channels=256,
-            conv_out_channels=256,
-            num_classes=1,
-            loss_mask=dict(
-                type='CrossEntropyLoss', use_mask=True, loss_weight=1.0))),
+            feat_channels=256,
+            anchor_generator=dict(
+                type='AnchorGenerator',
+                scales=[8],
+                ratios=[0.5, 1.0, 2.0],
+                strides=[4, 8, 16, 32, 64]),
+            bbox_coder=dict(
+                type='DeltaXYWHBBoxCoder',
+                target_means=[0.0, 0.0, 0.0, 0.0],
+                target_stds=[1.0, 1.0, 1.0, 1.0]),
+            loss_cls=dict(
+                type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
+            loss_bbox=dict(
+                type='SmoothL1Loss', beta=0.1111111111111111,
+                loss_weight=1.0)),
+        roi_head=dict(
+            type='CascadeRoIHead',
+            reg_roi_scale_factor=1.3,
+            num_stages=3,
+            stage_loss_weights=[1, 0.5, 0.25],
+            bbox_roi_extractor=dict(
+                type='SingleRoIExtractor',
+                roi_layer=dict(
+                    type='RoIAlign', output_size=7, sampling_ratio=0),
+                out_channels=256,
+                featmap_strides=[4, 8, 16, 32]),
+            bbox_head=[
+                dict(
+                    type='DoubleConvFCBBoxHead',
+                    num_convs=4,
+                    num_fcs=2,
+                    in_channels=256,
+                    conv_out_channels=1024,
+                    fc_out_channels=1024,
+                    roi_feat_size=7,
+                    num_classes=1,
+                    bbox_coder=dict(
+                        type='DeltaXYWHBBoxCoder',
+                        target_means=[0.0, 0.0, 0.0, 0.0],
+                        target_stds=[0.1, 0.1, 0.2, 0.2]),
+                    reg_class_agnostic=True,
+                    loss_cls=dict(
+                        type='CrossEntropyLoss',
+                        use_sigmoid=False,
+                        loss_weight=1.0),
+                    loss_bbox=dict(
+                        type='SmoothL1Loss', beta=1.0, loss_weight=1.0)),
+                dict(
+                    type='DoubleConvFCBBoxHead',
+                    num_convs=4,
+                    num_fcs=2,
+                    in_channels=256,
+                    conv_out_channels=1024,
+                    fc_out_channels=1024,
+                    roi_feat_size=7,
+                    num_classes=1,
+                    bbox_coder=dict(
+                        type='DeltaXYWHBBoxCoder',
+                        target_means=[0.0, 0.0, 0.0, 0.0],
+                        target_stds=[0.05, 0.05, 0.1, 0.1]),
+                    reg_class_agnostic=True,
+                    loss_cls=dict(
+                        type='CrossEntropyLoss',
+                        use_sigmoid=False,
+                        loss_weight=1.0),
+                    loss_bbox=dict(
+                        type='SmoothL1Loss', beta=1.0, loss_weight=1.0)),
+                dict(
+                    type='DoubleConvFCBBoxHead',
+                    num_convs=4,
+                    num_fcs=2,
+                    in_channels=256,
+                    conv_out_channels=1024,
+                    fc_out_channels=1024,
+                    roi_feat_size=7,
+                    num_classes=1,
+                    bbox_coder=dict(
+                        type='DeltaXYWHBBoxCoder',
+                        target_means=[0.0, 0.0, 0.0, 0.0],
+                        target_stds=[0.033, 0.033, 0.067, 0.067]),
+                    reg_class_agnostic=True,
+                    loss_cls=dict(
+                        type='CrossEntropyLoss',
+                        use_sigmoid=False,
+                        loss_weight=1.0),
+                    loss_bbox=dict(
+                        type='SmoothL1Loss', beta=1.0, loss_weight=1.0))
+            ],
+            mask_roi_extractor=dict(
+                type='SingleRoIExtractor',
+                roi_layer=dict(
+                    type='RoIAlign', output_size=14, sampling_ratio=0),
+                out_channels=256,
+                featmap_strides=[4, 8, 16, 32]),
+            mask_head=dict(
+                type='FCNMaskHead',
+                num_convs=4,
+                in_channels=256,
+                conv_out_channels=256,
+                num_classes=1,
+                loss_mask=dict(
+                    type='CrossEntropyLoss', use_mask=True, loss_weight=1.0))),
         train_cfg=dict(
-        rpn=dict(
-            assigner=dict(
-                type='MaxIoUAssigner',
-                pos_iou_thr=0.7,
-                neg_iou_thr=0.3,
-                min_pos_iou=0.3,
-                match_low_quality=True,
-                ignore_iof_thr=-1),
-            sampler=dict(
-                type='RandomSampler',
-                num=256,
-                pos_fraction=0.5,
-                neg_pos_ub=-1,
-                add_gt_as_proposals=False),
-            allowed_border=0,
-            pos_weight=-1,
-            debug=False),
-        rpn_proposal=dict(
-            nms_pre=2000,
-            max_per_img=2000,
-            nms=dict(type='nms', iou_threshold=0.7),
-            min_bbox_size=0),
-        rcnn=[
-            dict(
-                assigner=dict(
-                    type='MaxIoUAssigner',
-                    pos_iou_thr=0.5,
-                    neg_iou_thr=0.5,
-                    min_pos_iou=0.5,
-                    match_low_quality=False,
-                    ignore_iof_thr=-1),
-                sampler=dict(
-                    type='RandomSampler',
-                    num=512,
-                    pos_fraction=0.25,
-                    neg_pos_ub=-1,
-                    add_gt_as_proposals=True),
-                mask_size=28,
-                pos_weight=-1,
-                debug=False),
-            dict(
-                assigner=dict(
-                    type='MaxIoUAssigner',
-                    pos_iou_thr=0.6,
-                    neg_iou_thr=0.6,
-                    min_pos_iou=0.6,
-                    match_low_quality=False,
-                    ignore_iof_thr=-1),
-                sampler=dict(
-                    type='RandomSampler',
-                    num=512,
-                    pos_fraction=0.25,
-                    neg_pos_ub=-1,
-                    add_gt_as_proposals=True),
-                mask_size=28,
-                pos_weight=-1,
-                debug=False),
-            dict(
+            rpn=dict(
                 assigner=dict(
                     type='MaxIoUAssigner',
                     pos_iou_thr=0.7,
-                    neg_iou_thr=0.7,
-                    min_pos_iou=0.7,
-                    match_low_quality=False,
+                    neg_iou_thr=0.3,
+                    min_pos_iou=0.3,
+                    match_low_quality=True,
                     ignore_iof_thr=-1),
                 sampler=dict(
                     type='RandomSampler',
-                    num=512,
-                    pos_fraction=0.25,
+                    num=256,
+                    pos_fraction=0.5,
                     neg_pos_ub=-1,
-                    add_gt_as_proposals=True),
-                mask_size=28,
+                    add_gt_as_proposals=False),
+                allowed_border=0,
                 pos_weight=-1,
-                debug=False)
-        ]),
-    test_cfg=dict(
-        rpn=dict(
-            nms_pre=1000,
-            max_per_img=1000,
-            nms=dict(type='nms', iou_threshold=0.7),
-            min_bbox_size=0),
-        rcnn=dict(
-            score_thr=0.05,
-            nms=dict(type='nms', iou_threshold=0.5),
-            max_per_img=100,
-            mask_thr_binary=0.5)),
+                debug=False),
+            rpn_proposal=dict(
+                nms_pre=2000,
+                max_per_img=2000,
+                nms=dict(type='nms', iou_threshold=0.7),
+                min_bbox_size=0),
+            rcnn=[
+                dict(
+                    assigner=dict(
+                        type='MaxIoUAssigner',
+                        pos_iou_thr=0.5,
+                        neg_iou_thr=0.5,
+                        min_pos_iou=0.5,
+                        match_low_quality=False,
+                        ignore_iof_thr=-1),
+                    sampler=dict(
+                        type='RandomSampler',
+                        num=512,
+                        pos_fraction=0.25,
+                        neg_pos_ub=-1,
+                        add_gt_as_proposals=True),
+                    mask_size=28,
+                    pos_weight=-1,
+                    debug=False),
+                dict(
+                    assigner=dict(
+                        type='MaxIoUAssigner',
+                        pos_iou_thr=0.6,
+                        neg_iou_thr=0.6,
+                        min_pos_iou=0.6,
+                        match_low_quality=False,
+                        ignore_iof_thr=-1),
+                    sampler=dict(
+                        type='RandomSampler',
+                        num=512,
+                        pos_fraction=0.25,
+                        neg_pos_ub=-1,
+                        add_gt_as_proposals=True),
+                    mask_size=28,
+                    pos_weight=-1,
+                    debug=False),
+                dict(
+                    assigner=dict(
+                        type='MaxIoUAssigner',
+                        pos_iou_thr=0.7,
+                        neg_iou_thr=0.7,
+                        min_pos_iou=0.7,
+                        match_low_quality=False,
+                        ignore_iof_thr=-1),
+                    sampler=dict(
+                        type='RandomSampler',
+                        num=512,
+                        pos_fraction=0.25,
+                        neg_pos_ub=-1,
+                        add_gt_as_proposals=True),
+                    mask_size=28,
+                    pos_weight=-1,
+                    debug=False)
+            ]),
+        test_cfg=dict(
+            rpn=dict(
+                nms_pre=1000,
+                max_per_img=1000,
+                nms=dict(type='nms', iou_threshold=0.7),
+                min_bbox_size=0),
+            rcnn=dict(
+                score_thr=0.05,
+                nms=dict(type='nms', iou_threshold=0.5),
+                max_per_img=100,
+                mask_thr_binary=0.5)),
         _scope_='mmdet'))
 train_pipeline = [
     dict(
@@ -311,7 +324,7 @@ env_cfg = dict(
 randomness = dict(seed=None)
 default_hooks = dict(
     timer=dict(type='IterTimerHook'),
-    logger=dict(type='LoggerHook', interval=5),
+    logger=dict(type='LoggerHook', interval=10),
     param_scheduler=dict(type='ParamSchedulerHook'),
     checkpoint=dict(type='CheckpointHook', interval=1, save_best='auto'),
     sampler_seed=dict(type='DistSamplerSeedHook'),
@@ -326,7 +339,7 @@ default_hooks = dict(
 log_level = 'INFO'
 log_processor = dict(type='LogProcessor', window_size=10, by_epoch=True)
 load_from = None
-resume = False
+resume = True
 val_evaluator = dict(type='HmeanIOUMetric')
 test_evaluator = dict(type='HmeanIOUMetric')
 vis_backends = [dict(type='LocalVisBackend')]
@@ -384,7 +397,7 @@ train_dataloader = dict(
                            'scale_factor', 'flip_direction'))
         ]))
 val_dataloader = dict(
-    batch_size=,
+    batch_size=4,
     num_workers=1,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=False),
@@ -437,5 +450,4 @@ test_dataloader = dict(
         ]))
 auto_scale_lr = dict(base_batch_size=2)
 launcher = 'none'
-work_dir = '/content/drive/MyDrive/OCR/ckpt/augmented/cascade-doublehead-oclip-augumented.py'
-
+work_dir = './work-dirs/augumented-cascade-dh-clip/'
